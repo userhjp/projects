2e090251d4e866e2967ab691207d1045d43d7084
userhjp <819502953@qq.com>
2018-04-14 02:00:17 +0800

    删除重复

--- yunapp/src/src/assets/lib/util/localStorage.js
+++
@@ -1,9 +0,0 @@
-
-export default {
-    setStor:function (key,val){
-        window.localStorage.setItem(key,JSON.stringify(val));
-   },
-    getStor:function (){
-       return JSON.parse(window.localStorage.setItem(JSON.stringify(item))||[]);
-   }
-}
