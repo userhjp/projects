8e2d4d02c8062bb482f8eb264fc41687cafed318
userhjp <819502953@qq.com>
2018-04-16 11:04:00 +0800

    debug

--- yunapp/src/components/upload.vue
+++ yunapp/src/components/upload.vue
@@ -72,7 +72,6 @@ export default {
       fileArray.forEach((file)=>{
         formData.append("file",file);//循环遍历把文件对象插到formData对象上
       });
-      debugger
       let config = {  
         headers: {  
           'Content-Type': 'multipart/form-data' ,
