dfabc82b89563629dd68cfcaf4c704e26ea2dfea
userhjp <819502953@qq.com>
2018-04-25 15:58:47 +0800

    111

--- APIServer/routes/index.js
+++ APIServer/routes/index.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use('/news', require('../protect/controllers/newsController'));
   app.use('/article', require('../protect/controllers/articleController'));
   app.use('/files', require('../protect/uploads'));
-  // 404 page
+  // 404 page.
   // app.use(function (req, res,next) {
   //   if (!res.headersSent) {
   //     res.status(404).render('404')
