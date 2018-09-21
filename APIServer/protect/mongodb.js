5fa7305ecd4e917f5acafceb0063930a5e493358
userhjp <819502953@qq.com>
2018-09-21 11:04:28 +0800

    commit

--- APIServer/protect/mongodb.js
+++ APIServer/protect/mongodb.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost/test",function(err) {
+mongoose.connect("mongodb://localhost:27017/test", {useNewUrlParser:true},function(err) {
     if(err){
         console.log('连接失败');
     }else{
